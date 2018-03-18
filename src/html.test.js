6892a5d027c8845e9befa4017e4a3bb8b89c2c0c
Mario Fernandez <mario.fernandez@thoughtworks.com>
2018-03-18 21:36:36 +0100

    use case sensitive name for file

--- src/html.test.js
+++ src/html.test.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import { shallow } from 'enzyme'
-import HTML from './HTML'
+import HTML from './html'
 import Helmet from 'react-helmet'
 
 describe('components', () => {
