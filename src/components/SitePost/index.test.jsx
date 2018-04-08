eb414a42a40739bd6956e8f21a710a3b416e94f8
Mario Fernandez <mario.fernandez@thoughtworks.com>
2018-04-08 19:03:48 +0200

    Use snapshot test for blog post

--- src/components/SitePost/index.test.jsx
+++ src/components/SitePost/index.test.jsx
@@ -1,13 +1,21 @@
 import React from 'react'
 import { shallow } from 'enzyme'
+import toJson from 'enzyme-to-json'
 import SitePost from './index'
 
 describe('components', () => {
   describe('SitePost', () => {
     it('renders correctly', () => {
-      const data = { html: '<div></div>' }
+      const frontmatter = {
+        categories: ['JavaScript', 'React'],
+        date: '2018/03/01',
+        layout: 'post',
+        path: '/the-post',
+        title: 'My Post',
+      }
+      const data = { html: '<div>content</div>', frontmatter }
       const component = shallow(<SitePost data={data} />)
-      expect(component).toHaveLength(1)
+      expect(toJson(component)).toMatchSnapshot()
     })
   })
 })
