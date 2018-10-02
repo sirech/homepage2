8e5dafd987ebc144fcf52568e45b9ac4f32ba19b
Mario Fernandez <mario.fernandez@thoughtworks.com>
2018-10-02 12:34:54 +0200

    Make jest tests work again

--- jest.config.js
+++ jest.config.js
@@ -3,6 +3,9 @@ module.exports = {
     '\\.(jpg|jpeg|png|svg|woff|woff2)$': '<rootDir>/__mocks__/fileMock.js',
     '^.*\\.s?css$': 'identity-obj-proxy',
   },
+  transform: {
+    '^.+\\.jsx?$': './jest/transformer.js',
+  },
   setupFiles: ['<rootDir>/setupTests.js'],
   testPathIgnorePatterns: ['/node_modules/', '<rootDir>/.cache/'],
   globals: {
