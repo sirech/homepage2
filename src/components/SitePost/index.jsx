7d0ff96f4ea32e1d438a1ad2fc0987c6bdb29a7c
Mario Fernandez <mario.fernandez@thoughtworks.com>
2018-10-02 12:57:16 +0200

    Revert "Import links directly from gatsby"
    This reverts commit 1b5229bf9dfd8ebd47ffeca8a12c4ef2b1fc7900.

--- src/components/SitePost/index.jsx
+++ src/components/SitePost/index.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import cx from 'classnames'
 
-import { Link } from 'gatsby'
+import Link from 'gatsby-link'
 import forEach from 'lodash/forEach'
 import get from 'lodash/get'
 import ReadNext from '../ReadNext'
