635a466719789eceb51c0c4c94fff8d5a1e38965
Mario Fernandez <mario.fernandez@thoughtworks.com>
2019-07-27 18:30:11 +0200

    add csp configuration

--- gatsby-config.js
+++ gatsby-config.js
@@ -134,6 +134,22 @@ module.exports = {
         respectDNT: true,
       },
     },
+    {
+      resolve: `gatsby-plugin-csp`,
+      options: {
+        disableOnDev: true,
+        reportOnly: false, // Changes header to Content-Security-Policy-Report-Only for csp testing purposes
+        mergeScriptHashes: true, // you can disable scripts sha256 hashes
+        mergeStyleHashes: true, // you can disable styles sha256 hashes
+        mergeDefaultDirectives: true,
+        directives: {
+          'script-src': "'self' www.google-analytics.com",
+          'style-src': "'self' 'unsafe-inline'",
+          'img-src': "'self' data: www.google-analytics.com",
+          'connect-src': "'self' https://www.google-analytics.com/j/collect",
+        },
+      },
+    },
     'gatsby-plugin-catch-links',
     'gatsby-plugin-react-helmet',
     'gatsby-plugin-sass',
