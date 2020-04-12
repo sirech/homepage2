c029b51a18709af553adeeec6e09294f46b13704
Mario Fernandez <mario.fernandez@hceris.com>
2020-04-12 22:38:15 +0200

    switch page creation to await/async syntax

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,70 +1,65 @@
-const Promise = require('bluebird')
 const path = require('path')
 const R = require('ramda')
 
 const createPaginatedPages = require('gatsby-paginate')
 
-exports.createPages = ({ graphql, actions }) => {
+exports.createPages = async ({ graphql, actions, reporter }) => {
   const { createPage } = actions
 
-  return new Promise((resolve, reject) => {
-    resolve(
-      graphql(
-        `
-          {
-            site {
-              siteMetadata {
+  const result = await graphql(
+    `
+      {
+        site {
+          siteMetadata {
+            title
+            description
+            author
+            url: siteUrl
+            twitter
+          }
+        }
+        allMarkdownRemark(
+          sort: { fields: [frontmatter___date], order: DESC }
+          filter: { frontmatter: { draft: { eq: false } } }
+        ) {
+          posts: edges {
+            post: node {
+              html
+              frontmatter {
+                layout
                 title
+                path
+                categories
+                date(formatString: "YYYY/MM/DD")
+                draft
                 description
-                author
-                url: siteUrl
-                twitter
-              }
-            }
-            allMarkdownRemark(
-              sort: { fields: [frontmatter___date], order: DESC }
-              filter: { frontmatter: { draft: { eq: false } } }
-            ) {
-              posts: edges {
-                post: node {
-                  html
-                  frontmatter {
-                    layout
-                    title
-                    path
-                    categories
-                    date(formatString: "YYYY/MM/DD")
-                    draft
-                    description
-                  }
-                }
               }
             }
           }
-        `
-      ).then((result) => {
-        if (result.errors) {
-          console.log(result.errors)
-          reject(result.errors)
         }
+      }
+    `
+  )
 
-        createPaginatedPages({
-          edges: result.data.allMarkdownRemark.posts,
-          createPage: createPage,
-          pageTemplate: 'src/templates/blog-index.js',
-          pageLength: 10,
-          pathPrefix: 'blog',
-          context: result.data.site,
-        })
+  if (result.errors) {
+    reporter.panicOnBuild(`Error while running GraphQL query.`)
+    return
+  }
 
-        // Create blog posts pages.
-        R.forEach((edge) => {
-          createPage({
-            path: edge.post.frontmatter.path,
-            component: path.resolve('./src/templates/blog-post.js'),
-          })
-        })(result.data.allMarkdownRemark.posts)
-      })
-    )
+  createPaginatedPages({
+    edges: result.data.allMarkdownRemark.posts,
+    createPage: createPage,
+    pageTemplate: 'src/templates/blog-index.js',
+    pageLength: 10,
+    pathPrefix: 'blog',
+    context: result.data.site,
   })
+
+  // Create blog posts pages.
+  R.forEach((edge) => {
+    createPage({
+      path: edge.post.frontmatter.path,
+      component: path.resolve('./src/templates/blog-post.js'),
+    })
+  })(result.data.allMarkdownRemark.posts)
 }
