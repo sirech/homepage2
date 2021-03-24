57ae98bc4f3a6e02495a47c93d8493e25f2ed2b5
Mario Fernandez <mario.fernandez@hceris.com>
2021-03-24 01:33:48 +0100

    fix graphql warning

--- src/templates/blog-post.js
+++ src/templates/blog-post.js
@@ -1,6 +1,8 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 
+import { graphql } from 'gatsby'
+
 import SitePost from '../components/SitePost'
 import SEO from '../components/SEO'
 
