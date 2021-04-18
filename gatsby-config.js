d04293f6c76baf59e1ad16f8be04cfaf2abf7454
Mario Fernandez <mario.fernandez@hceris.com>
2021-04-18 12:34:05 +0200

    remove incompatible bundle analyzer plugin

--- gatsby-config.js
+++ gatsby-config.js
@@ -178,7 +178,6 @@ module.exports = {
     'gatsby-plugin-favicon',
     'gatsby-plugin-layout',
     'gatsby-transformer-sharp',
-    'gatsby-plugin-webpack-bundle-analyzer',
     'gatsby-plugin-eslint',
   ],
 }
