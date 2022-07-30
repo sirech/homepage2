eaa1b151e3b247ef09c045e1073cd0af43be4941
Mario Fernandez <mario.fernandez@hceris.com>
2022-07-30 22:00:41 +0200

    fix e2e configuration

--- cypress.config.js
+++ cypress.config.js
@@ -1,6 +1,11 @@
-const { defineConfig } = require('cypress')
+const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
-  projectId: '65dg6c'
-})
+  projectId: "65dg6c",
 
+  e2e: {
+    setupNodeEvents(on, config) {
+      // implement node event listeners here
+    },
+  },
+});
